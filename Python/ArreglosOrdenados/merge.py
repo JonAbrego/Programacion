c8f2070fe4abd6df269210232c4bb1b6d9a1ae1f
Jonathan <jo.ab.al@ciencias.unam.mx>
2020-09-11 18:52:51 -0500

    mix two organized array

---
+++ Python/ArreglosOrdenados/merge.py
@@ -0,0 +1,43 @@
+def merge(a, b):
+	c = []
+	arreglo1 = 0
+	arreglo2 = 0
+	i=0	
+	while( (arreglo1 < len(a)) & (arreglo2 < len(b)) ):
+		if(a[arreglo1] < b[arreglo2]):						
+			c.append(a[arreglo1])
+			arreglo1+=1
+			i+=1
+		else:
+			c.append(b[arreglo2])
+			arreglo2+=1
+			i+=1		
+
+	#cuando un arreglo ya termino y faltan elementos de otro
+	while (arreglo1 < len(a)):
+		c.append(a[arreglo1])
+		arreglo1+=1;
+
+	while (arreglo2 < len(b)):
+		c.append(b[arreglo2])	
+		arreglo2+=1;		
+	print(c)	
+
+merge([1,3,5],[2,4,6])
+
+merge([3, 4, 9, 9, 17, 20], [8, 9, 40, 41])
+#returns [3, 4, 8, 9, 9, 9, 17, 20, 40, 41]
+
+merge([5, 6, 20], [2, 3, 4, 5])
+#returns [2, 3, 4, 5, 5, 6, 20]
+
+merge([1, 3, 5, 7, 9], [2, 4, 6, 8])
+#returns [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+merge([14, 17, 18, 21, 22, 26], [3, 15, 30, 31])
+#returns [3, 14, 15, 17, 18, 21, 22, 26, 30, 31]
+
+merge([3, 4, 6, 9, 11, 16, 17], [8, 9, 9, 40])
+#returns [3, 4, 6, 8, 9, 9, 9, 11, 16, 17, 40]
+
+			
